04acce95509948e204fbb28c517975c851b66a08
jomallaru <jomallaru@gmail.com>
2023-08-11 10:14:17 -0500

    update comunidad

--- scripts/comentarios.js
+++ scripts/comentarios.js
@@ -1,55 +1,52 @@
-function guardarBlog() {
-    let formData = new FormData();
-    formData.append('nombre', $("#nombre").val());
-    formData.append('fecha', $("#fecha").val());
-    formData.append('texto', $("#texto").val());
-    formData.append('imagen', $("#imagen")[0].files[0]); // Obtener el archivo de imagen seleccionado
-    
-    $.ajax({
-      url: "https://g1e315ff777a567-cocinar.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/comentario/blog",
-      type: "POST",
-      data: formData,
-      processData: false, // Indicar que no se procese el contenido de formData
-      contentType: false, // Indicar que no se establezca el tipo de contenido automáticamente
-      success: function (respuesta) {
-        alert("Su comentario guardado correctamente");
-        $("#nombre").val("");
-        $("#fecha").val("");
-        $("#texto").val("");
-        $("#imagen").val(""); // Limpiar el campo de entrada de imagen
-      },
-      error: function (xhr, status) {
-        alert("Error al guardar el comentario");
-      }
-    });
-  }
-  
-  
+function TraerComentarios() {
+  $.ajax({
+    url: "https://g1e315ff777a567-cocinar.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/comentario/blog",
+    type: "GET",
+    dataType: "json",
+    success: function (respuesta) {
+      mostrarBlog(respuesta.items);
+    },
+    error: function (xhr, status) {
+      alert("Error de Petición");
+    }
+  });
+}
+
+function mostrarBlog(Blog) {
+  let tabla = $("#tabla");
+  tabla.empty();
+  tabla.append("<tr><th>Receta</th><th>Ingredientes</th><th>Preparación</th><th>Dificultad</th><th>Eliminar</th><th>Editar</th></tr>");
+  let botonEliminar = "<button type=\"button\" class=\"btn-eliminar\" onclick=\"eliminarComentario('" + Blog[i].Receta + "')\">Eliminar</button>";
+
+  let botonEditar = "<button type=\"button\" onclick=\"editarComentario('" + Blog[i].Receta + "', '" + Blog[i].Ingredientes + "', '" + Blog[i].Preparacion + "', '" + Blog[i].Dificultad + "')\">Editar</button>";
+  tabla.append("<tr><td>" + Blog[i].Receta + "</td><td>" + Blog[i].Ingredientes + "</td><td>" + Blog[i].Preparacion + "</td><td>" + Blog[i].Dificultad + "</td><td>" + botonEliminar + "</td><td>" + botonEditar + "</td></tr>");
+}
 
 
+function GuardarBlog() {
+  let Blog = {
+    'Receta': $("#nombre").val(),
+    'Ingredientes': $("#ingredientes").val(),
+    'Preparacion': $("#preparacion").val(),
+    'Dificultad': $("#dificultad").val(),
+    'Imagen': $("#imagen").val()
+  };
 
-
-function guardarComentario() {
-    let crearComentario = {
-      'nombre': $("#nombre").val(),
-      'fecha': $("#fecha").val(),
-      'texto': $("#texto").val()
-    };
-  
-    $.ajax({
-      url: "https://g1e315ff777a567-cocinar.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/comentario/blog",
-      type: "POST",
-      data: crearComentario,
-      dataType: "json",
-      success: function (respuesta) {
-        alert("Comentario guardado correctamente");
-        $("#nombre").val("");
-        $("#fecha").val("");
-        $("#texto").val("");
-      },
-      error: function (xhr, status) {
-        alert("Error al guardar el comentario");
-      }
-    });
-  }
-  
+  $.ajax({
+    url: "https://g1e315ff777a567-cocinar.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/comentario/blog",
+    type: "POST",
+    data: Blog,
+    dataType: "json",
+    success: function (respuesta) {
+      alert("Comentario creado correctamente"); 
+      $("#nombre").val("");
+      $("#ingredientes").val("");
+      $("#preparacion").val("");
+      $("#dificultad").val("");
+      $("#imagen").val("");
+    },
+    error: function (xhr, status) {
+      alert("Error");
+    }
+  });
+}
