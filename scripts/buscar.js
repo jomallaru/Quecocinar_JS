da906a148ea720b23572c01ec1cff64163f28da9
jomallaru <jomallaru@gmail.com>
2024-10-21 22:08:17 -0500

    update 2024

--- scripts/buscar.js
+++ scripts/buscar.js
@@ -1,48 +1,46 @@
 function buscarRecetas() {
-    const ingrediente = document.getElementById('ingrediente').value.toLowerCase();
-    const cardsContainer = document.getElementById("cards-container");
-    cardsContainer.innerHTML = ''; // Limpiar contenedor de cards
+  const ingrediente = document.getElementById('ingrediente').value.toLowerCase();
+  const cardsContainer = document.getElementById("cards-container");
+  cardsContainer.innerHTML = ''; // Limpiar contenedor de cards
 
-    // Hacer petición a la API para obtener las recetas
-    fetch('https://gb2ca086f6748de-s27aub565ndywxqi.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/recetas/recetas')
-        .then(response => response.json())
-        .then(data => {
-            // Filtrar las recetas que contengan el ingrediente ingresado
-            const recetasFiltradas = data.items.filter(receta =>
-                receta.ingredientes.toLowerCase().includes(ingrediente)
-            );
+  // Hacer petición a la API para obtener las recetas
+  fetch('https://gb2ca086f6748de-s27aub565ndywxqi.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/recetas/recetas')
+    .then(response => response.json())
+    .then(data => {
+      // Filtrar las recetas que contengan el ingrediente ingresado
+      const recetasFiltradas = data.items.filter(receta =>
+        receta.ingredientes.toLowerCase().includes(ingrediente)
+      );
 
-            // Mostrar las recetas filtradas en forma de cards
-            mostrarRecetas(recetasFiltradas);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Error de Petición');
-        });
+      // Mostrar las recetas filtradas en forma de cards
+      mostrarRecetas(recetasFiltradas);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Error de Petición');
+    });
 }
 
 
 
 function mostrarRecetas(recetas) {
-    let htmlparaagregar = "";
-  
-    if (recetas.length === 0) {
-      alert("No se encontró receta con ese ingrediente");
-    } else {
-      for (let i = 0; i < recetas.length; i++) {
-        htmlparaagregar += '<div class="card">';
-        htmlparaagregar += '<h2>' + recetas[i].nombre + '</h2>';
-        htmlparaagregar += '<li><strong>Ingredientes:</strong> ' + recetas[i].ingredientes + '</li>';
-        htmlparaagregar += '<p>' + recetas[i].preparacion + '</p>';
-        htmlparaagregar += '<ul>';
-       
-        htmlparaagregar += '<li><strong>Tiempo:</strong> ' + recetas[i].tiempo + ' minutos</li>';
-        htmlparaagregar += '<li><strong>Dificultad:</strong> ' + recetas[i].dificultad + '</li>';
-        htmlparaagregar += '</ul>';
-        htmlparaagregar += '</div>';
-      }
-  
-      document.getElementById("cards-container").innerHTML = htmlparaagregar;
+  let htmlparaagregar = "";
+
+  if (recetas.length === 0) {
+    alert("No se encontró receta con ese ingrediente");
+  } else {
+    for (let i = 0; i < recetas.length; i++) {
+      htmlparaagregar += '<div class="card">';
+      htmlparaagregar += '<h2>' + recetas[i].nombre + '</h2>';
+      htmlparaagregar += '<li><strong>Ingredientes:</strong> ' + recetas[i].ingredientes + '</li>';
+      htmlparaagregar += '<p>' + recetas[i].preparacion + '</p>';
+      htmlparaagregar += '<ul>';
+      htmlparaagregar += '<li><strong>Tiempo:</strong> ' + recetas[i].tiempo + ' minutos</li>';
+      htmlparaagregar += '<li><strong>Dificultad:</strong> ' + recetas[i].dificultad + '</li>';
+      htmlparaagregar += '</ul>';
+      htmlparaagregar += '</div>';
     }
+
+    document.getElementById("cards-container").innerHTML = htmlparaagregar;
   }
-  
+}
